6541c5cc50640fc13246991f70d369c76f9cdf1d
Bijoy Kar <bijoykar54321@gmail.com>
2025-03-09 13:49:45 +0530

    update: Add setup functionality to create luna.local.json

--- setup.cjs
+++ setup.cjs
@@ -2,6 +2,7 @@
 // ? doskey luna=node "<main.js file path>" $*
 
 const { spawn } = require("child_process");
+const fs = require("fs");
 
 const setup = () => {
   const setupCommand = `doskey luna=node main.js $*`;
@@ -11,9 +12,10 @@ const setup = () => {
   setupProcess.on("exit", (code) => {
     process.exit(code);
   });
+  fs.writeFileSync("luna.local.json", JSON.stringify({ defaultModel: "" }));
+  console.log("Setup complete!");
 };
 
 // * Setup the Luna CLI
 // Todo: Run `node setup.js` to setup the Luna CLI
 setup();
-console.log(__filename);
